b7ae75aa24bf1101544e92b886fa2e32b5a261c1
Xabi08YT <xabigoity@gmail.com>
2023-09-05 21:27:50 +0200

    Advanced code

--- NativeLibs/var.py
+++ NativeLibs/var.py
@@ -1,22 +1,58 @@
+variables = {}
+
 class charactère:
-    def __init__(self):
+    def __init__(self, name:str, readonly:bool = False):
         self.val = ""
+        self.readonly = readonly
+        self.name = name
         return
         
 
     def assign(self,val:str):
+        assert self.readonly, f"Error, {self.name} is readonly var"
         if len(val)>1:
             raise Exception("Cannot assign more than one character in a charactère var type.")
         self.val = val
 
 
 class entier:
-    def __init__(self):
+    def __init__(self, name:str, readonly:bool = False):
         self.val = None
+        self.readonly = readonly
+        self.name = name
         return
     
 
     def assign(self,val:int):
+        assert self.readonly, f"Error, {self.name} is readonly var"
+        self.val = val
+
+
+class réel:
+    def __init__(self, name:str, readonly:bool = False):
+        self.val=None
+        self.readonly = readonly
+        self.name = name
+        return
+    
+
+    def assign(self,val:float):
+        assert self.readonly, f"Error, {self.name} is readonly var"
+        self.val = val
+
+
+class booléen:
+    def __init__(self, name:str, readonly:bool = False):
+        self.val = None
+        self.readonly = readonly
+        self.name = name
+        return
+    
+
+    def assign(self,val):
+        assert self.readonly, f"Error, {self.name} is readonly var"
+        if val != "Vrai" or val != "Faux":
+            raise Exception("Invalid value for this var type.")
         self.val = val
 
 
--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import NativeLibs
+
+
+instructionSet = {"var ": NativeLibs.var.var, "Si": None}
+_currentblocks = []
+_currentindent = 0
+requireblock = ["Fonction","Si","Sinon","Faire","Pour","Tant"]
+
+
+def execLine(line:str):
+    global _currentindent
+    splitline = line.split(" ")
+    if splitline[0] in instructionSet and splitline[0] not in requireblock:
+        instructionSet[splitline[0]](line.replace(str(splitline[0]+" "),""))
+    elif splitline[0] in requireblock:
+        _currentindent += 1
+        _currentblocks.append(_currentindent)
+    
+
+
+
 def execfile(path):
     with open(path, mode="rb") as f:
         content = f.readlines()
@@ -6,8 +27,8 @@ def execfile(path):
     code = []
 
     for line in content:
-        code.append(line.decode("utf-8")) 
-    
+        code.append(line.decode("utf-8"))
+        execLine(code)
 
 
 if __name__ == "__main__":
